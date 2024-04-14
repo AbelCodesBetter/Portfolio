5f3ed966ee7ded09a02a4ce7a019a014eaf92ea3
AbelCodesBetter <163462133+AbelCodesBetter@users.noreply.github.com>
2024-04-14 20:32:08 +0200

    Add files via upload

---
+++ Tutor Game/Tutor Game/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Tutor_Game
+    /*
+     * Author: Abel Mahlobogoane
+     * Date: 14 December 2023
+     * Purpose: Creating a tutoring game that will help users improve their spelling
+     */
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+    
+        private void InitializeComponent()
+        {
+            this.labelSentence = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.listBoxAccuracy = new System.Windows.Forms.ListBox();
+            this.buttonNewGame = new System.Windows.Forms.Button();
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelSentence
+            // 
+            this.labelSentence.AutoSize = true;
+            this.labelSentence.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSentence.Location = new System.Drawing.Point(18, 14);
+            this.labelSentence.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelSentence.Name = "labelSentence";
+            this.labelSentence.Size = new System.Drawing.Size(151, 29);
+            this.labelSentence.TabIndex = 0;
+            this.labelSentence.Text = "Hello World";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInput.Location = new System.Drawing.Point(24, 66);
+            this.textBoxInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(478, 35);
+            this.textBoxInput.TabIndex = 1;
+            this.textBoxInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBoxInput_KeyPress);
+            // 
+            // listBoxAccuracy
+            // 
+            this.listBoxAccuracy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxAccuracy.FormattingEnabled = true;
+            this.listBoxAccuracy.ItemHeight = 25;
+            this.listBoxAccuracy.Location = new System.Drawing.Point(24, 137);
+            this.listBoxAccuracy.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listBoxAccuracy.Name = "listBoxAccuracy";
+            this.listBoxAccuracy.Size = new System.Drawing.Size(478, 254);
+            this.listBoxAccuracy.TabIndex = 2;
+            this.listBoxAccuracy.SelectedIndexChanged += new System.EventHandler(this.listBoxAccuracy_SelectedIndexChanged);
+            // 
+            // buttonNewGame
+            // 
+            this.buttonNewGame.Location = new System.Drawing.Point(24, 438);
+            this.buttonNewGame.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonNewGame.Name = "buttonNewGame";
+            this.buttonNewGame.Size = new System.Drawing.Size(180, 46);
+            this.buttonNewGame.TabIndex = 3;
+            this.buttonNewGame.Text = "New Game";
+            this.buttonNewGame.UseVisualStyleBackColor = true;
+            this.buttonNewGame.Click += new System.EventHandler(this.buttonNewGame_Click);
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Location = new System.Drawing.Point(324, 438);
+            this.buttonQuit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(180, 46);
+            this.buttonQuit.TabIndex = 4;
+            this.buttonQuit.Text = "Quit";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 503);
+            this.Controls.Add(this.buttonQuit);
+            this.Controls.Add(this.buttonNewGame);
+            this.Controls.Add(this.listBoxAccuracy);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.labelSentence);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Typing Tutor Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelSentence;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.ListBox listBoxAccuracy;
+        private System.Windows.Forms.Button buttonNewGame;
+        private System.Windows.Forms.Button buttonQuit;
+    }
+}
+
---
+++ Tutor Game/Tutor Game/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Tutor_Game
+/*
+* Author: Abel Mahlobogoane
+* Date: 14 December 2023
+* Purpose: Creating a tutoring game that will help users improve their spelling
+*/
+{
+    public partial class Form1 : Form
+    {
+        // Array of sentences for the game
+        private string[] sentences = { "Hello World", "Typing", "C# Programming", "Visual Studio", "Coding", "Eduvos" };
+
+        // Random number generator for selecting sentences
+        private Random random = new Random();
+
+        // Current sentence being displayed
+        private string currentSentence;
+
+        // Index of the current sentence in the array
+        private int currentIndex;
+
+        // List to store correctly typed letters
+        private List<char> correctLetters = new List<char>();
+
+        // Dictionary to store accuracy information for each letter
+        private Dictionary<char, int> accuracyTable = new Dictionary<char, int>();
+
+        // Form constructor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Event handler for the form load event
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize and start a new game when the form loads
+            StartNewGame();
+        }
+
+        // Method to start a new game
+        private void StartNewGame()
+        {
+            try
+            {
+                // Select a random sentence and initialize game variables
+                currentIndex = random.Next(sentences.Length);
+                currentSentence = sentences[currentIndex];
+                DisplaySentence();
+                correctLetters.Clear();
+                accuracyTable.Clear();
+                UpdateAccuracyListBox();
+                textBoxInput.Text = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                HandleError("Error starting a new game", ex);
+            }
+        }
+
+        // Method to display the current sentence
+        private void DisplaySentence()
+        {
+            labelSentence.Text = currentSentence;
+        }
+
+        // Method to update the accuracy information in the ListBox
+        private void UpdateAccuracyListBox()
+        {
+            listBoxAccuracy.Items.Clear();
+            try
+            {
+                // Iterate through each letter in the current sentence
+                foreach (char letter in currentSentence)
+                {
+                    // Get accuracy information and add it to the ListBox
+                    int totalAttempts = accuracyTable.ContainsKey(letter) ? accuracyTable[letter] : 0;
+                    int successes = correctLetters.Count(c => c == letter);
+                    listBoxAccuracy.Items.Add($"{letter}: {successes}/{totalAttempts}");
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleError("Error updating accuracy list box", ex);
+            }
+        }
+
+        // Event handler for key press in the input TextBox
+        private void TextBoxInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                // Check if Enter key is pressed to evaluate accuracy
+                if (e.KeyChar == (char)Keys.Return)
+                {
+                    CheckAccuracy();
+                    textBoxInput.Text = string.Empty;
+                    DisplaySentence();
+                }
+                else
+                {
+                    // Record accuracy for each key press
+                    RecordAccuracy(e.KeyChar);
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleError("Error processing key press", ex);
+            }
+        }
+
+        // Method to check the accuracy of the input
+        private void CheckAccuracy()
+        {
+            bool perfectScore = true;
+
+            try
+            {
+                // Sets to keep track of processed and remaining letters
+                HashSet<char> processedLetters = new HashSet<char>();
+                HashSet<char> remainingLetters = new HashSet<char>(currentSentence);
+
+                // Iterate through each letter in the input
+                foreach (char letter in textBoxInput.Text)
+                {
+                    if (!processedLetters.Contains(letter))
+                    {
+                        // Update accuracy information and check correctness
+                        int totalAttempts = accuracyTable.ContainsKey(letter) ? accuracyTable[letter] : 0;
+                        int successes = correctLetters.Count(c => c == letter);
+                        accuracyTable[letter] = totalAttempts + 1;
+
+                        processedLetters.Add(letter);
+
+                        if (remainingLetters.Contains(letter))
+                        {
+                            correctLetters.Add(letter);
+                            remainingLetters.Remove(letter);
+                        }
+                        else
+                        {
+                            perfectScore = false; // Excess letter typed, mark as incorrect
+                        }
+
+                        // Check if there is any letter with less than perfect score
+                        if (successes < totalAttempts)
+                        {
+                            perfectScore = false;
+                        }
+                    }
+                }
+
+                // Update accuracy information in the ListBox
+                UpdateAccuracyListBox();
+
+                // Check if the user achieved a perfect score
+                if (perfectScore && remainingLetters.Count == 0)
+                {
+                    MessageBox.Show("You win! Perfect score!", "Congratulations", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    StartNewGame();
+                }
+                else
+                {
+                    MessageBox.Show("Try again", "Incorrect", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleError("An unexpected error occurred while checking accuracy", ex);
+            }
+        }
+
+        // Method to record accuracy for a specific letter
+        private void RecordAccuracy(char letter)
+        {
+            try
+            {
+                if (!accuracyTable.ContainsKey(letter))
+                {
+                    accuracyTable[letter] = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleError("Error recording accuracy", ex);
+            }
+        }
+
+        // Event handler for the "New Game" button click
+        private void buttonNewGame_Click(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+
+        // Event handler for the "Quit" button click
+        private void buttonQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        // Event handler for ListBox selection change 
+        private void listBoxAccuracy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Method to handle errors and display error messages
+        private void HandleError(string message, Exception ex)
+        {
+            // Handle unexpected errors
+            MessageBox.Show($"{message}\n\nDetails: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
